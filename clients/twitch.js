dc399601c2f217af77d64881c219c574172f83b6
imverbatim <ajdelco22@gmail.com>
2020-04-07 12:39:07 -0400

    commands added and are avaliable. ask command and all static commands working

--- clients/twitch.js
+++ clients/twitch.js
@@ -9,8 +9,7 @@ module.exports = (function () {
         constructor() {
             this.client = new DankTwitch.ChatClient({
 				username: process.env.BOTNAME,
-				password: process.env.OAUTH,
-				rateLimits: 30
+				password: process.env.OAUTH
             }); 
 
             console.log(process.env.OAUTH)
@@ -39,7 +38,19 @@ module.exports = (function () {
             });
             client.on("PRIVMSG", (privMessage) => { this.handleMessage(privMessage); });
             client.on("WHISPER", (message) => { this.handleMessage(message) });
-            client.on("USERNOTICE", async(messageObject) => { /* ... */ })
+            client.on("USERNOTICE", async(messageObject) => { /* ... */ });
+            client.on("USERSTATE", async (messageObject) => {
+                const { emoteSets } = messageObject;
+				const strings = [
+					emoteSets.sort().join(","),
+					(this.availableEmoteSets)
+						? this.availableEmoteSets.sort().join(",")
+						: null
+				];
+            });
+            client.on("NOTICE", async (messageObject) => {});
+            client.on("CLEARCHAT", (messageObject) => {});
+
 
         }
 
@@ -47,8 +58,43 @@ module.exports = (function () {
             const {ircTags, badges, bits, channelName, messageText: message, senderUserID, senderUsername} = messageObject;
 			const messageType = (messageObject instanceof DankTwitch.WhisperMessage)
 				? "whisper"
-				: "message";
+                : "message";
+                
+            let channelData = null;
+			const userData = await hb.User.get(senderUsername, false);
+			if (!userData) {
+				return;
+            }
+            // Check and execute command if necessary
+			if (message.startsWith('#')) {
+				let userState = {};
+				if (messageType === "message") {
+					userState = messageObject.extractUserState();
+				}
 
+                const [command, ...args] = message.replace(/^\$\s*/, "$").split(" ");
+                const result = await this.handleCommand(
+                    command, 
+                    senderUsername,
+                    channelName,
+                    args,
+                    {
+						userBadges: userState.badges,
+						userBadgeInfo: userState.badgeInfo,
+						color: userState.color,
+						colorRaw: userState.colorRaw,
+						privateMessage: (messageType === "whisper")
+					}
+                );
+            }
         }
-    }
+
+        async handleCommand(command, user, channel, args = [], options = {}) {
+            const userData = await hb.User.get(user, false);
+            const channelData = await hb.Channel.get(channel);
+            
+            var res = await hb.Command.checkExec(command, args, user, channel, options);
+            this.client.say(channel, res);
+        }
+    }   
 })();
